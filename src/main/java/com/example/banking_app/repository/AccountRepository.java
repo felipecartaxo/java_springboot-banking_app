891139801c062c8471e76f420ae11b93e69ca96a
Neraky <felipecartaxo1@gmail.com>
2024-09-03 11:44:00 -0300

    editing a comment

--- src/main/java/com/example/banking_app/repository/AccountRepository.java
+++ src/main/java/com/example/banking_app/repository/AccountRepository.java
@@ -3,7 +3,7 @@ package com.example.banking_app.repository;
 import com.example.banking_app.entities.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-// Classe responsável pelas operações de CRUD da classe Account
+// Classe responsável pelas operações de persistência
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
 }
