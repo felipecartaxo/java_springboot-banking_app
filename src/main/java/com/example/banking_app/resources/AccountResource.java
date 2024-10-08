2ab9376e7c304c53942bc048637ce7609c466949
Neraky <felipecartaxo1@gmail.com>
2024-10-08 14:20:27 -0300

    update and delete method

--- src/main/java/com/example/banking_app/resources/AccountResource.java
+++ src/main/java/com/example/banking_app/resources/AccountResource.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Map;
 
 @RestController // Indica que esta classe é um REST Controller
@@ -13,35 +14,48 @@ import java.util.Map;
 public class AccountResource { // Classe que disponibiliza as regras de negócio como endpoints de uma API RESTful
 
     @Autowired // Injeta automaticamente a dependência do serviço AccountService, que contém a lógica das regras de negócio
-    private AccountService accountService;
+    private AccountService service;
 
     @PostMapping // Este método será chamado quando uma requisição HTTP do tipo POST for feita para /api/accounts. É utilizado para criar uma nova conta
     public Account createAccount(@RequestBody Account account) { // Determina que o corpo da requisição será transformado em uma instância da classe Account e passado para o método
-        return accountService.createAccount(account); // Retorna a conta criada
+        return service.createAccount(account); // Retorna a conta criada
+    }
+
+    // Requisição do tipo GET que retorna todas as contas registradas
+    @GetMapping
+    public List<Account> getAllAccounts() {
+        return service.findAll();
     }
 
     @GetMapping("/{id}") // Responde a requisições GET feitas para /api/accounts/{id}. Serve para obter os detalhes de uma conta a partir de um id
     public Account getAccount(@PathVariable Long id) { // O id da conta é extraído da URL e passado como argumento
-        return accountService.getAccount(id).orElseThrow(() -> new RuntimeException("Account not found")); // Busca a conta correspondente ao id fornecido. Se a conta não for encontrada, uma exceção RuntimeException
+        return service.getAccount(id).orElseThrow(() -> new RuntimeException("Account not found")); // Busca a conta correspondente ao id fornecido. Se a conta não for encontrada, uma exceção RuntimeException
     }
 
     @PostMapping("{id}/deposit") // Responde a requisições POST para /api/accounts/{id}/deposit. Usado para realizar depósitos em uma conta específica
     public Account deposit(@PathVariable Long id, @RequestBody Map<String, Double> request) { // Extrai o id da conta a partir da URL. O corpo da requisição contém um Map de chave-valor, onde a chave é "amount" e o valor é o próprio valor do depósito
         Double amount = request.get("amount"); // O método extrai o valor associado à chave "amount" no request body
 
-        return accountService.deposit(id, amount); // Chama o serviço accountService.deposit(id, amount), que executa a lógica do depósito. Após a operação, a conta atualizada é retornada
+        return service.deposit(id, amount); // Chama o serviço accountService.deposit(id, amount), que executa a lógica do depósito. Após a operação, a conta atualizada é retornada
     }
 
     @PostMapping("{id}/withdraw") // @PostMapping("{id}/withdraw"): Responde a requisições POST para /api/accounts/{id}/withdraw. Usado para realizar saques em uma conta específica
     public Account withdraw (@PathVariable Long id, @RequestBody Map<String, Double> request) { // Extrai o id da conta a partir da URL. O corpo da requisição contém um Map de chave-valor, onde a chave é "amount" e o valor é o próprio valor do saque
         Double amount = request.get("amount"); // O método extrai o valor associado à chave "amount" no request body
 
-        return accountService.withdraw(id, amount); // Chama o serviço accountService.withdraw(id, amount), que executa a lógica do saque. Após a operação, a conta atualizada é retornada
+        return service.withdraw(id, amount); // Chama o serviço accountService.withdraw(id, amount), que executa a lógica do saque. Após a operação, a conta atualizada é retornada
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Account> update (@PathVariable Long id, @RequestBody Account obj) {
+        obj = service.update(id, obj);
+
+        return ResponseEntity.ok().body(obj);
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete (@PathVariable Long id) {
-        accountService.delete(id);
+        service.delete(id);
 
         return ResponseEntity.noContent().build();
     }
--- src/main/java/com/example/banking_app/service/AccountService.java
+++ src/main/java/com/example/banking_app/service/AccountService.java
@@ -6,61 +6,63 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service // Diz para o Spring que esta é a classe que contém a lógica principal das regras de negócio da nossa aplicação
 public class AccountService {
 
     @Autowired // Mecanismo de injeção de dependência do Spring. Associa uma instância de AccountRepository na classe AccountService
-    public AccountRepository accountRepository; // Injeção de dependência
+    public AccountRepository repository; // Injeção de dependência
 
-    // Salva uma conta
+    // Cria e salva uma conta
     public Account createAccount(Account account) {
-        return accountRepository.save(account);
+        return repository.save(account);
+    }
+
+    // Retorna todas as contas registradas
+    public List<Account> findAll() {
+        return repository.findAll();
     }
 
     // Retorna uma conta a partir de um id
     public Optional<Account> getAccount(Long id) {
-        // Considere lançar uma exceção caso a conta não seja encontrada
-
-        return accountRepository.findById(id);
+        return repository.findById(id);
     }
 
     // Realiza o depósito em uma conta. Aqui iremos passar o id da conta que receberá o depósito e o valor do depósito
     public Account deposit(Long id, double amount) {
-        // Busca a conta associada ao id acima e verifica se o Optional<Account> contém um valor. Se não tiver, lança uma exceção
-        Account account = getAccount(id).orElseThrow(() -> new RuntimeException("Account not found"));
-        // Atualiza o saldo da conta
-        account.setBalance(account.getBalance() + amount);
+        Account account = getAccount(id) // Busca a conta associada ao id acima e verifica se o Optional<Account> contém um valor
+                .orElseThrow(() -> new RuntimeException("Account not found")); // Se não tiver, lança uma exceção
 
-        // Salva a conta atualizada no banco de dados e, após ser salvo no banco, retorna como resultado do método
-        return accountRepository.save(account);
+        account.setBalance(account.getBalance() + amount); // Atualiza o saldo da conta
+
+        return repository.save(account); // Salva a conta atualizada no banco de dados e, após ser salvo no banco, retorna como resultado do método
     }
 
     // Realiza o saque em uma conta. De forma análoga ao método acima, aqui iremos passar também o id da conta onde realizaremos o saque e o valor do saque
     public Account withdraw(Long id, double amount) {
-        // Busca a conta associada ao id passado como parâmetro do método e verifica se o Optional<Account> contém um valor. Se não contiver, lança uma exceção
-        Account account = getAccount(id).orElseThrow(() -> new RuntimeException("Account not found"));
-        // Verifica se a conta possui saldo suficiente para a realização do saque
-        if (account.getBalance() < amount) {
+        Account account = getAccount(id).orElseThrow(() -> new RuntimeException("Account not found")); // Busca a conta associada ao id passado como parâmetro do método e verifica se o Optional<Account> contém um valor. Se não contiver, lança uma exceção
+        if (account.getBalance() < amount) { // Verifica se a conta possui saldo suficiente para a realização do saque
             throw new RuntimeException("Insufficient funds");
         }
-        // Caso possua saldo suficiente, então atualiza o saldo da conta
-        account.setBalance(account.getBalance() - amount);
 
-        // Salva a conta atualizada no banco de dados e, após ser salvo no banco, retorna como resultado do método
-        return accountRepository.save(account);
+        account.setBalance(account.getBalance() - amount); // Caso possua saldo suficiente, então atualiza o saldo da conta
+
+        return repository.save(account); // Após a operação ser concluída, a conta será salva com seu saldo atualizado
     }
 
+    // Delete uma conta a partir de um id
     public void delete(Long id) {
-        try {
-            accountRepository.deleteById(id); // Se a conta existir, a mesma será apagada
-        }
-        catch (EmptyResultDataAccessException e) { // Caso a conta não exista
-            System.out.println("Account not found");
-        }
-        catch(Exception e) { // Lança exceção genérica
-            System.out.println("An error has occurred");
-        }
+        Account account = getAccount(id).orElseThrow(() -> new RuntimeException("Account not found"));
+
+        repository.deleteById(account.getId());
+    }
+
+    public Account update(Long id, Account obj) {
+        Account entity = repository.getReferenceById(id);
+        entity.setAccountHolderName(obj.getAccountHolderName());
+
+        return repository.save(entity);
     }
 }
